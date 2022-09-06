a43b5014cbe9436e0d340caed8f1cd97c97d8a44
alexandrefernandesjs <alexandrefernandesjs@gmail.com>
2022-09-06 14:45:18 -0300

    update

--- lib/src/core/services/dot_env/dot_env_service.dart
+++ lib/src/core/services/dot_env/dot_env_service.dart
@@ -3,14 +3,21 @@ import 'dart:io';
 class DotEnvService {
   final Map<String, String> _map = {};
   static DotEnvService instance = DotEnvService._();
-  DotEnvService._();
+  DotEnvService._() {
+    _init();
+  }
 
   void _init() {
     final file = File('.env');
-    final envText = file.readAssStringSync();
+    final envText = file.readAsStringSync();
 
     for (var line in envText.split('\n')) {
       final lineBreak = line.split('=');
+      _map[lineBreak[0]] = lineBreak[1];
     }
+  }
+
+  String? operator [](String key) {
+    return _map[key];
   }
 }
