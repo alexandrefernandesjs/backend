cbe9cc0dafef07fb7f175d8d96981c1504a270a9
alexandrefernandesjs <alexandrefernandesjs@gmail.com>
2022-09-09 15:07:41 -0300

    finalizando crud de clients

--- lib/src/features/clients/clients_resource.dart
+++ lib/src/features/clients/clients_resource.dart
@@ -1,5 +1,5 @@
-import 'dart:convert';
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:backend/src/core/services/database/remote_database.dart';
 import 'package:shelf/shelf.dart';
@@ -10,41 +10,69 @@ class ClientResource extends Resource {
   // TODO: implement routes
   List<Route> get routes => [
         Route.get('/clients', _getAllClients),
-        Route.get('/client/:id', _getProductByid),
-        Route.post('/client', _createProduct),
-        Route.put('/client', _updateProduct),
-        Route.delete('/client/:id', _deteleProduct),
+        Route.get('/client/:id', _getClientByid),
+        Route.post('/client', _createClient),
+        Route.put('/client', _updateClient),
+        Route.delete('/client/:id', _deteleClient),
       ];
 
   FutureOr<Response> _getAllClients(Injector injector) async {
     final database = injector.get<RemoteDatabase>();
 
     final result = await database
-        .query('SELECT id, nome, unidade, role, preco	FROM public."Product"');
-    final productMap = result.map((element) => element['Product']).first;
-    return Response.ok(jsonEncode(productMap));
+        .query('SELECT id, nome, role, cc, cpf, limite FROM "Client";');
+    // final productMap = result.map((element) => element['Product']).first;
+    return Response.ok(jsonEncode(result));
   }
 
-  FutureOr<Response> _getProductByid(
+  FutureOr<Response> _getClientByid(
+      ModularArguments arguments, Injector injector) async {
+    final id = arguments.params['id'];
+    final database = injector.get<RemoteDatabase>();
+    final result = await database.query(
+        'SELECT id, nome, role, cc, cpf, limite FROM "Client" WHERE id = @id;',
+        variables: {'id': id});
+    final clientMap = result.map((element) => element['Client']).first;
+    return Response.ok(jsonEncode(clientMap));
+  }
+
+  FutureOr<Response> _createClient(
+      ModularArguments arguments, Injector injector) async {
+    final clientParams = (arguments.data as Map).cast<String, dynamic>();
+    clientParams.remove('id');
+
+    final database = injector.get<RemoteDatabase>();
+    final insertClient = await database.query(
+        'INSERT INTO "Client"(nome, cc, cpf, limite)VALUES (@nome, @cc, @cpf, @limite) RETURNING id, nome, cc, cpf, limite, role;',
+        variables: clientParams);
+    final clientMap = insertClient.map((element) => element['Client']).first;
+
+    return Response.ok(jsonEncode(clientMap));
+  }
+
+  FutureOr<Response> _updateClient(
+      ModularArguments arguments, Injector injector) async {
+    final clientPArams = (arguments.data as Map).cast<String, dynamic>();
+
+    final columns = clientPArams.keys
+        .where((key) => key != 'id' || key != 'role')
+        .map((key) => '$key=@$key')
+        .toList();
+    final database = injector.get<RemoteDatabase>();
+    final updateClient = await database.query(
+        'UPDATE "Client"SET ${columns.join(',')} WHERE id = @id RETURNING id, nome, cc, cpf, limite, role;',
+        variables: clientPArams);
+    final clientMap = updateClient.map((element) => element['Client']).first;
+
+    return Response.ok(jsonEncode(clientMap));
+  }
+
+  FutureOr<Response> _deteleClient(
       ModularArguments arguments, Injector injector) async {
     final id = arguments.params['id'];
     final database = injector.get<RemoteDatabase>();
     final result = await database
-        .query('SELECT id, nome, unidade, role, preco	FROM public."Product"');
-    final productMap = result.map((element) => element['Product']).first;
-
-    return Response.ok(jsonEncode(productMap));
-  }
-
-  FutureOr<Response> _createProduct(ModularArguments arguments) {
-    return Response.ok('Create Product ${arguments.data}');
-  }
-
-  FutureOr<Response> _updateProduct(ModularArguments arguments) {
-    return Response.ok('Updated Prodduct ${arguments.params['id']}');
-  }
-
-  FutureOr<Response> _deteleProduct(ModularArguments arguments) {
-    return Response.ok('Deleted Product ${arguments.params['id']}');
+        .query('DELETE FROM "Client" WHERE id = @id;', variables: {'id': id});
+    return Response.ok(jsonEncode({'message': 'deleted $id'}));
   }
 }
